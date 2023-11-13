3e96cd249bd02cfedc78e522abc7a78f3e95cf0d
OldBeardMan <147204540+OldBeardMan@users.noreply.github.com>
2023-11-13 20:22:41 +0100

    HOW DOES IT WORK AAAAAAAAAAA

--- homepage/views.py
+++ homepage/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render, redirect
 from .forms import SubscribeForm
 
 def home_view(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        form = SubscribeForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            # Tutaj możesz zapisać e-mail w pliku, wysłać go do zewnętrznego API itp.
+            with open('subscribers.txt', 'a') as f:
+                f.write(email + '\n')
+    else:
+        form = SubscribeForm()
+
+    return render(request, 'home.html', {'form': form})
+
