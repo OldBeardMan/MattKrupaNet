c1dc6513feafd7531731f36d099293c3a0d5fbdb
OldBeardMan <147204540+OldBeardMan@users.noreply.github.com>
2023-11-15 17:47:47 +0100

    AAAAAAAAAAAAAAAAAA

--- homepage/views.py
+++ homepage/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render, redirect
 from .forms import SubscribeForm
 
 def home_view(request):
+
     if request.method == 'POST':
         form = SubscribeForm(request.POST)
         if form.is_valid():
-            email = form.cleaned_data['email']
-            # Tutaj możesz zapisać e-mail w pliku, wysłać go do zewnętrznego API itp.
-            with open('subscribers.txt', 'a') as f:
-                f.write(email + '\n')
+            form.save()
+            return redirect('home')  # Utwórz widok potwierdzenia zapisu
     else:
         form = SubscribeForm()
 
     return render(request, 'home.html', {'form': form})
+
 
