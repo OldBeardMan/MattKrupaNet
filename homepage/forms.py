c5986d18f9118394443bbf17072fd0fb5218c3d4
OldBeardMan <147204540+OldBeardMan@users.noreply.github.com>
2023-11-17 17:47:42 +0100

    Done the newsleter form

--- homepage/forms.py
+++ homepage/forms.py
@@ -5,3 +5,12 @@ class SubscribeForm(forms.ModelForm):
     class Meta:
         model = Subscriber
         fields = ['email']
+        widgets = {
+            'email': forms.EmailInput(attrs={
+                'class': 'custom-input',
+                'placeholder': 'Some awesome person'
+            }),
+        }
+        labels = {
+            'email': '',
+        }
